23504955c38489e86beeddd480e74f0df1979580
Garnett05 <charlleslimamota10@hotmail.com>
2020-04-05 16:16:24 -0300

    Initial commit

---
+++ AppViews/AppViews/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppViews
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Master.MasterPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
